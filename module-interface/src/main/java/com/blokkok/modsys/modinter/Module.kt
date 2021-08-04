cc9ec64a04b5db3becec7449429b5668170976a1
Iyxan23 <nurihsanalghifari@gmail.com>
2021-08-04 09:00:35 +0700

    docs: add docs on functions on Module abstract class

--- module-interface/src/main/java/com/blokkok/modsys/modinter/Module.kt
+++ module-interface/src/main/java/com/blokkok/modsys/modinter/Module.kt
@@ -6,6 +6,13 @@ abstract class Module {
     abstract val namespace: String
     open val flags: List<String> = emptyList()
 
+    /**
+     * This function will be called when this current module is loaded
+     */
     abstract fun onLoaded(comContext: CommunicationContext)
+
+    /**
+     * This function will be called before this current module is unloaded
+     */
     abstract fun onUnloaded(comContext: CommunicationContext)
 }
